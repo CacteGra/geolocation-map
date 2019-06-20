03a31a43dd322953a59c9a16cc8c0cde369a1706
vig <cactegra@protonmail.com>
2019-06-20 17:24:04 +0800

    variable declared

--- vigilumbra/vigilumbra/settings.py
+++ vigilumbra/vigilumbra/settings.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'vigilumbra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = 
-								{ 
-															'default': dj_database_url.config( default=config('DATABASE_URL') ) 
-									}
+DATABASES = { 'default': dj_database_url.config( default=config('DATABASE_URL') ) }
 
 
 # Password validation

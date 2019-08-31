96ea3ea52303fc017c4a362b1b083d4dc8d8c061
vig <cactegra@protonmail.com>
2019-08-31 18:55:14 +0800

    type error

--- vigilumbra/vigilumbra/wsgi.py
+++ vigilumbra/vigilumbra/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'h.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vigilumbra.settings')
 
 application = get_wsgi_application()

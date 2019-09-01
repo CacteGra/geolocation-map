20bb0d81d0f2e24a6626b5de61133f5e654a3788
vig <cactegra@protonmail.com>
2019-09-01 10:03:48 +0800

    import views

--- mapping/urls.py
+++ mapping/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from mapping import MainPageView
+from mapping.views import MainPageView
 
 urlpatterns = [
     url(r'^$', MainPageView.as_view()),
